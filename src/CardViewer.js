6ba728a7cc2d2048cc1e811c517108a6e478a05a
Alex Yin <alex_yin1@hotmail.com>
2022-12-16 10:34:54 -0500

    2nd bootcamp mostly done

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,10 +1,49 @@
 import React from 'react';
 
 class CardViewer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 1
+    }
+  }
+
+  IncrementItem = () => {
+    if (this.state.count < this.props.cards.length) {
+      this.setState({ count: this.state.count + 1 });
+    }
+  }
+  
+  DecreaseItem = () => {
+    if (this.state.count > 1) {
+      this.setState({ count: this.state.count - 1 });
+    }
+  }
+
   render() {
+    const cards = this.props.cards.map((card) => {
+        return (
+            <div>
+                {card.front}
+            </div>
+        );
+      });
+    
     return (
       <div>
         <h2>Card Viewer</h2>
+        <div>
+            <button onClick={this.flip}>{cards[this.state.count-1]}</button>
+        </div>
+        <div>
+            <button onClick={this.DecreaseItem}>Previous</button>
+            <button onClick={this.IncrementItem}>Next</button>
+
+        </div>
+        <div>
+          Card {this.state.count}/{this.props.cards.length}
+        </div>
+
         <hr />
         <button onClick={this.props.switchMode}>Go to card editor</button>
       </div>
